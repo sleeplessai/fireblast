7b9ea7cd993636d819a43929950f7994e433c503
sleeplessai <2018063+sleeplessai@users.noreply.github.com>
2020-11-18 17:26:04 +0800

    experiment.loop tensorboard, stdout, docs

--- fireblast/experiment/loop.py
+++ fireblast/experiment/loop.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .default import FireblastExperiment
-from typing import Union
+from typing import Union, Tuple
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 
 __all__ = ['Loop']
@@ -18,8 +19,30 @@ class Loop:
     optimizer: torch.optim.Optimizer,
     scheduler: torch.optim.lr_scheduler._LRScheduler = None,
     criterion = F.cross_entropy,
-    device: Union[torch.device, str] = torch.device('cuda:0')
-  ):
+    device: Union[torch.device, str] = torch.device('cuda:0'),
+    epoch: int = -1,
+    summary_writer: torch.utils.tensorboard.SummaryWriter = None,
+    stdout_freq: int = 50,
+    **kwargs
+  ) -> Tuple[float, float]:
+    """
+    Universal one-epoch leaner loop for training an image classification neural network.
+
+    Args:
+      fbxo (FireblastExperiment, Dataloader): A FireblastExperiment or PyTorch DataLoader object for trainset iteration
+      model (torch.nn.Module): PyTorch nerual network model(network)
+      optimizer (torch.optim.Optimizer): PyTorch optimizer for training the given model
+      scheduler (torch.optim.lr_scheduler._LRScheduler): PyTorch lr_scheduler object to update learning rate dynamically
+      criterion (Callable): Loss function, cross entropy loss for image classification as default
+      device (torch.device, str): The device where the model is trained
+      epoch (int): Current training epoch
+      summary_writer (torch.utils.tensorboard.SummaryWriter): Tensorboard SummaryWriter records loss and accuracy
+      stdout_freq (int): Info stdout frequency, no info will be printed if set std_freq < 0
+
+    Returns:
+      train_loss, train_accuracy: Float classification metric pair of training loss and accuracy at this epoch
+
+    """
     if isinstance(fbxo, FireblastExperiment):
       assert fbxo.TrainsetLoader
       trainset_loader = fbxo.TrainsetLoader
@@ -33,6 +56,7 @@ class Loop:
     model.train()
     train_loss = 0.
     correct, total, count = 0, 0, 0
+    std_epoch = f'epoch: {epoch} | ' if epoch >= 0 else ''
     for index, (image, label) in enumerate(trainset_loader):
       optimizer.zero_grad()
       x, y = image.to(device), label.to(device)
@@ -41,25 +65,56 @@ class Loop:
       train_loss += loss.item()
       loss.backward()
       optimizer.step()
-      print(f'index: {index} | loss: {loss.item()}')
       # accuracy
       _, indices = torch.max(pred, dim=1)
       total += label.size(0)
       correct += indices.eq(y).cpu().sum()
       count = index
+      # stdout
+      if stdout_freq > 0 and index % stdout_freq == 0:
+        print(std_epoch + f'index: {index} | loss: {loss.item():.5f}')
+
     if scheduler: scheduler.step()
 
     train_loss /= (count + 1)
     train_accuracy = 100. * float(correct) / total
+
+    if stdout_freq > 0:
+      print(std_epoch + f'train_loss: {train_loss:.5f} | train_accuracy: {train_accuracy:.4f}%')
+    if summary_writer and epoch != -1:
+      summary_writer.add_scalar('Training/Loss/epoch', train_loss, epoch)
+      summary_writer.add_scalar('Training/Accuracy/epoch', train_accuracy, epoch)
+
     return train_loss, train_accuracy
+
 
   @staticmethod
   def validate(
     fbxo: Union[FireblastExperiment, DataLoader],
     model: nn.Module,
     criterion = F.cross_entropy,
-    device: Union[torch.device, str] = torch.device('cuda:0')
-  ):
+    device: Union[torch.device, str] = torch.device('cuda:0'),
+    epoch: int = -1,
+    summary_writer: torch.utils.tensorboard.SummaryWriter = None,
+    stdout_freq: int = 50,
+    **kwargs
+  ) -> Tuple[float, float]:
+    """
+    Universal validation loop for testing an image classification neural network.
+
+    Args:
+      fbxo (FireblastExperiment, Dataloader): A FireblastExperiment or PyTorch DataLoader object for testset iteration
+      model (torch.nn.Module): PyTorch nerual network model(network)
+      criterion (Callable): Loss function, cross entropy loss for image classification as default
+      device (torch.device, str): The device for the model validation
+      epoch (int): The epoch validation after training
+      summary_writer (torch.utils.tensorboard.SummaryWriter): Tensorboard SummaryWriter records loss and accuracy on testset
+      stdout_freq (int): Info stdout frequency, no info will be printed if set std_freq < 0
+
+    Returns:
+      val_loss, val_accuracy: Float classification metric pair of validation loss and accuracy on testset
+
+    """
     if isinstance(fbxo, FireblastExperiment):
       assert fbxo.TestsetLoader
       testset_loader = fbxo.TestsetLoader
@@ -71,19 +126,30 @@ class Loop:
     device = torch.device(device) if isinstance(device, str) else device
     model.to(device)
     model.eval()
-    test_loss = 0.
+    val_loss = 0.
     correct, total, count = 0, 0, 0
+    std_epoch = f'epoch: {epoch} | ' if epoch >= 0 else ''
     for index, (image, label) in enumerate(testset_loader):
       x, y = image.to(device), label.to(device)
       pred = model(x)
       loss = criterion(pred, y)
-      test_loss += loss.item()
+      val_loss += loss.item()
       # accuracy
       _, indices = torch.max(pred, dim=1)
       total += label.size(0)
       correct += indices.eq(y).cpu().sum()
       count = index
+      # stdout
+      if stdout_freq > 0 and index % stdout_freq == 0:
+        print(std_epoch + f'index: {index} | loss: {loss.item():.5f}')
 
-    test_loss /= (count + 1)
-    test_accuracy = 100. * float(correct) / total
-    return test_loss, test_accuracy
+    val_loss /= (count + 1)
+    val_accuracy = 100. * float(correct) / total
+
+    if stdout_freq > 0:
+      print(std_epoch + f'val_loss: {val_loss:.5f} | val_accuracy: {val_accuracy:.4f}%')
+    if summary_writer and epoch >= 0:
+      summary_writer.add_scalar('Validation/Loss/epoch', val_loss, epoch)
+      summary_writer.add_scalar('Validation/Accuracy/epoch', val_accuracy, epoch)
+
+    return val_loss, val_accuracy
